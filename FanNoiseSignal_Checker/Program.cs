31a630f925bca27ea2eba3823f769f34e3703a26
Jeremy Tsai <JETSAI@microsoft.com>
2024-01-08 20:36:51 +0800

    Fix typo and using streamwriter

--- FanNoiseSignal_Checker/Program.cs
+++ FanNoiseSignal_Checker/Program.cs
@@ -1,8 +1,8 @@
 namespace FanNoiseSignal_Checker
 {
+    using Microsoft.Windows.EventTracing;
     using System;
     using System.IO;
-    using Microsoft.Windows.EventTracing;
 
     internal class Program
     {
@@ -35,37 +35,50 @@ namespace FanNoiseSignal_Checker
 
         private static void ProcessFanNoiseSignal(string filePath)
         {
-            var resultFilePath = Path.GetFullPath(@"FanNoiseSignal_Result.txt");
+            var resultFilePath = Path.GetFullPath(@"FanNoiseSignal_Result"+ "_" + DateTime.Now.ToString("yyyyMMdd_HHmm") + ".txt");
 
-            var resultWriter = new StreamWriter(resultFilePath);
-            var trace = TraceProcessor.Create(filePath);
-            var pendingGenericEvent = trace.UseGenericEvents();
-
-            trace.Process();
-
-            var traceMetadata = trace.UseMetadata();
-            var traceStartTime = traceMetadata.StartTime;
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"Trace Start Time: {traceStartTime}");
-            resultWriter.WriteLine($"Trace Start Time: {traceStartTime}");
-            Console.ResetColor();
-            var genericEventData = pendingGenericEvent.Result;
-
-            foreach (var genericEvent in genericEventData.Events)
+            using (var resultWriter = new StreamWriter(resultFilePath))
             {
-                if (genericEvent.ProviderName == @"Microsoft.Windows.Kernel.Power" && genericEvent.TaskName == @"PopFanUpdateSpeed_UpdatedNoiseLevel")
-                {
-                    var timestamp = genericEvent.Timestamp.DateTimeOffset;
-                    var oldFanNoiseLevel = genericEvent.Fields[1].AsInt32;
-                    var newFanNoiseLevel = genericEvent.Fields[2].AsInt32;
+                var trace = TraceProcessor.Create(filePath);
+                var pendingGenericEvent = trace.UseGenericEvents();
 
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"Log Time: {timestamp}, OldFanNoiseLevel: {oldFanNoiseLevel}, NewFanNoiseLevel: {newFanNoiseLevel}");
-                    resultWriter.WriteLine($"Log Time: {timestamp}, OldFanNoiseLevel: {oldFanNoiseLevel}, NewFanNoiseLevel: {newFanNoiseLevel}");
+                trace.Process();
+
+                var traceMetadata = trace.UseMetadata();
+                var traceStartTime = traceMetadata.StartTime;
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Trace Start Time: {traceStartTime}");
+                resultWriter.WriteLine($"Trace Start Time: {traceStartTime}");
+                Console.ResetColor();
+                var genericEventData = pendingGenericEvent.Result;
+                bool fandata = false;
+
+                foreach (var genericEvent in genericEventData.Events)
+                {
+                    if (genericEvent.ProviderName == @"Microsoft.Windows.Kernel.Power" && genericEvent.TaskName == @"PopFanUpdateSpeed_UpdatedNoiseLevel")
+                    {
+                        var timestamp = genericEvent.Timestamp.DateTimeOffset;
+                        var oldFanNoiseLevel = genericEvent.Fields[1].AsInt32;
+                        var newFanNoiseLevel = genericEvent.Fields[2].AsInt32;
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Log Time: {timestamp}, OldFanNoiseLevel: {oldFanNoiseLevel}, NewFanNoiseLevel: {newFanNoiseLevel}");
+                        resultWriter.WriteLine($"Log Time: {timestamp}, OldFanNoiseLevel: {oldFanNoiseLevel}, NewFanNoiseLevel: {newFanNoiseLevel}");
+                        Console.ResetColor();
+                        fandata = true;
+                    }
+                }
+
+                if (!fandata)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("There is no Fan Noise Signal level change.");
+                    resultWriter.WriteLine("There is no Fan Noise Signal level change.");
                     Console.ResetColor();
                 }
             }
+
         }
     }
 }
