1fb2c3e151221bc00b84584b5347fd36bac92951
jetsai-msft <61871839+tsjeremy@users.noreply.github.com>
2024-03-10 12:42:42 +0800

    Refactor console output and add result writer

--- FanNoiseSignal_Checker/Program.cs
+++ FanNoiseSignal_Checker/Program.cs
@@ -6,7 +6,7 @@ namespace FanNoiseSignal_Checker
 {
     internal class Program
     {
-        private static readonly Guid Microsoft_Windows_Kernel_Power = Guid.Parse("63bca7a1-77ec-4ea7-95d0-98d3f0c0ebf7");
+        private static readonly Guid Microsoft_Windows_Kernel_Power = Guid.Parse("63bca7a1-77ec-4ea7-95d0-98d3f0c0ebf7"); // Microsoft-Windows-Kernel-Power provider GUID
         private static readonly string UpdatedNoiseLevel = @"PopFanUpdateSpeed_UpdatedNoiseLevel";
         private static readonly string TripPoint = @"PopFanUpdateSpeed_TripPoint";
 
@@ -49,12 +49,7 @@ namespace FanNoiseSignal_Checker
                 var trace = TraceProcessor.Create(filePath, tps);
 
                 var traceMetadata = trace.UseMetadata();
-                Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"Trace Start Time:\t{traceMetadata.StartTime}");
-                resultWriter.WriteLine($"Trace Start Time:\t{traceMetadata.StartTime}");
-                Console.ResetColor();
-                Console.WriteLine();
+                WriteResult(resultWriter, $"Trace Start Time:\t{traceMetadata.StartTime}", ConsoleColor.Yellow);
 
                 var pendingKernelPowerEvent = trace.UseGenericEvents(Microsoft_Windows_Kernel_Power);
 
@@ -62,7 +57,7 @@ namespace FanNoiseSignal_Checker
 
                 if (pendingKernelPowerEvent.HasResult == false)
                 {
-                    Console.Error.WriteLine(@"No Microsoft.Windows.Kernel.Power events found in the trace.");
+                    WriteResult(resultWriter, @"No Microsoft.Windows.Kernel.Power events found in the trace.", ConsoleColor.Red);
                     return;
                 }
 
@@ -77,10 +72,7 @@ namespace FanNoiseSignal_Checker
                         var oldFanNoiseLevel = genericEvent.Fields[1].AsInt32;
                         var newFanNoiseLevel = genericEvent.Fields[2].AsInt32;
 
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine($"Log Time:\t\t{timestamp}: OldFanNoiseLevel: {oldFanNoiseLevel}, NewFanNoiseLevel: {newFanNoiseLevel}");
-                        resultWriter.WriteLine($"Log Time:\t\t{timestamp}: OldFanNoiseLevel: {oldFanNoiseLevel}, NewFanNoiseLevel: {newFanNoiseLevel}");
-                        Console.ResetColor();
+                        WriteResult(resultWriter, $"Log Time:\t\t{timestamp}: OldFanNoiseLevel: {oldFanNoiseLevel}, NewFanNoiseLevel: {newFanNoiseLevel}", ConsoleColor.Green);
                         fandata = true;
                     }
                     else if (genericEvent.TaskName == TripPoint)
@@ -89,23 +81,25 @@ namespace FanNoiseSignal_Checker
                         var lowTripPoint = genericEvent.Fields[1].AsUInt32;
                         var highTripPoint = genericEvent.Fields[2].AsUInt32;
 
-                        Console.ForegroundColor = ConsoleColor.Cyan;
-                        Console.WriteLine($"Log Time:\t\t{timestamp}: LowTripPoint: {lowTripPoint}, HighTripPoint: {highTripPoint}");
-                        resultWriter.WriteLine($"Log Time:\t\t{timestamp}: LowTripPoint: {lowTripPoint}, HighTripPoint: {highTripPoint}"); 
-                        Console.ResetColor();
-                        fandata = true;
+                        WriteResult(resultWriter, $"Log Time:\t\t{timestamp}: LowTripPoint: {lowTripPoint}, HighTripPoint: {highTripPoint}", ConsoleColor.Cyan);
                         Console.WriteLine();
+                        fandata = true;
                     }   
                 }
 
                 if (!fandata)
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("There is no Fan Noise Signal level change.");
-                    resultWriter.WriteLine("There is no Fan Noise Signal level change.");
-                    Console.ResetColor();
+                    WriteResult(resultWriter, "There is no Fan Noise Signal level change.", ConsoleColor.Red);
                 }
             }
+        }
+
+        private static void WriteResult(StreamWriter writer, string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            writer.WriteLine(message);
+            Console.ResetColor();
         }
     }
 }
